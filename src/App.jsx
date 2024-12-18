13960a78a7617476fca71d49828289e681778d10
Kumail Ahmad <144327213+kumail-ahmad@users.noreply.github.com>
2024-12-18 10:46:09 +0530

    fixed UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import { v4 as uuidv4 } from "uuid";
+import { MdModeEdit } from "react-icons/md";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 function App() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
+  const [showFinished, setshowFinished] = useState(true);
 
   useEffect(() => {
     let todoString = localStorage.getItem("todos");
@@ -18,8 +22,12 @@ function App() {
     localStorage.setItem("todos", JSON.stringify(todos));
   };
 
+  const toggleFinished = (params) => {
+    setshowFinished(!showFinished);
+  };
+
   const handleEdit = (e, id) => {
-    console.log(e, id);
+    // console.log(e, id);
     let t = todos.filter((i) => i.id === id);
     setTodo(t[0].todo);
     let newTodos = todos.filter((item) => {
@@ -71,22 +79,35 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto my-5 rounded-xl p-5 bg-gray-200 min-h-[80vh] min-w-fit">
+      <div className="md:container md:mx-auto my-5 rounded-xl p-5 bg-gray-200 min-h-[80vh] md:w-1/2">
+        <h1 className="font-bold text-center text-3xl">
+          iPlan-Plan your activities at one place
+        </h1>
         <div className="addTodo my-5">
           <h2 className="text-lg font-bold">Add a Todo</h2>
-          <input
-            onChange={handleChange}
-            value={todo}
-            type="text"
-            className="w-[60vh] rounded-md"
-          />
-          <button
-            onClick={handleAdd}
-            className="bg-gray-500 hover:bg-gray-900 p-2 py-1 text-sm font-bold text-white rounded-md mx-6"
-          >
-            Save
-          </button>
+          <div className="flex">
+            <input
+              onChange={handleChange}
+              value={todo}
+              type="text"
+              className=" rounded-md w-full"
+            />
+            <button
+              onClick={handleAdd}
+              disabled={todo.length < 3}
+              className="disabled:bg-gray-400 bg-gray-700 hover:bg-gray-900 p-2 py-1 text-sm font-bold text-white rounded-md mx-6"
+            >
+              Save
+            </button>
+          </div>
         </div>
+        <input
+          onChange={toggleFinished}
+          type="checkbox"
+          checked={showFinished}
+        />
+        <span>Show Finished ({todos.length})</span>
+        <div className="hr my-3 h-[2px] opacity-40 bg-black "></div>
         <h2 className="text-lg font-bold">Your todos</h2>
         <div className="todos">
           {todos.length === 0 && (
@@ -94,46 +115,50 @@ function App() {
           )}
           {todos.map((item) => {
             return (
-              <div
-                key={item.id}
-                className="todo flex w-1/4 my-3 justify-between"
-              >
-                <div className="Both-together flex gap-5">
-                  <input
-                    name={item.id}
-                    type="checkbox"
-                    value={item.isCompleted}
-                    className="checkbox"
-                    onChange={handleCheckbox}
-                    id=""
-                  />
-                  <div className={item.isCompleted ? "line-through" : ""}>
-                    {item.todo}
+              (showFinished || !item.isCompleted) && (
+                <div key={item.id} className="todo flex  my-3 justify-between">
+                  <div className="Both-together items-center flex gap-5">
+                    <input
+                      name={item.id}
+                      type="checkbox"
+                      checked={item.isCompleted}
+                      className="checkbox"
+                      onChange={handleCheckbox}
+                      id=""
+                    />
+                    <div
+                      className={`${
+                        item.isCompleted ? "line-through " : ""
+                      }md:w-full     sm:w-1/2    flex items-center justify-between overflow-hidden truncate`}
+                    >
+                      {item.todo}
+                    </div>
+                  </div>
+                  <div className="buttons flex gap-2 h-full">
+                    <button
+                      onClick={(e) => {
+                        handleEdit(e, item.id);
+                      }}
+                      className=" bg-gray-500 hover:bg-gray-900 p-3 py-2 text-sm font-bold text-white rounded-md mx-1"
+                    >
+                      <MdModeEdit />
+                    </button>
+                    <button
+                      onClick={(e) => {
+                        handleDelete(e, item.id);
+                      }}
+                      className="bg-gray-500 hover:bg-gray-900 p-3 py-2 text-sm font-bold text-white rounded-md mx-1"
+                    >
+                      <RiDeleteBin6Line />
+                    </button>
                   </div>
                 </div>
-                <div className="buttons flex gap-2 h-full">
-                  <button
-                    onClick={(e) => {
-                      handleEdit(e, item.id);
-                    }}
-                    className=" bg-gray-500 hover:bg-gray-900 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
-                  >
-                    Edit
-                  </button>
-                  <button
-                    onClick={(e) => {
-                      handleDelete(e, item.id);
-                    }}
-                    className="bg-gray-500 hover:bg-gray-900 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
+              )
             );
           })}
         </div>
       </div>
+      <Footer />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,24 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <nav>
-        <div className="logo">
-          <span className="font-bold text-xl mx-9">iPlan</span>
-        </div>
-        <a className="hover:font-bold" href="/">
-          <li>Home</li>
-        </a>
-        <a className="hover:font-bold" href="/">
-          <li>About </li>
-        </a>
-
-        <li>
-          <a className="hover:font-bold transition-all" href="/">
-            Contact
+      <nav className=" bg-gray-700 rounded-md list-none">
+        <div className="container md:w-3/2 flex gap-5 items-center text-white p-3 ">
+          <div className="logo">
+            <span className="font-bold text-2xl mx-9">iPlan</span>
+          </div>
+          <a className="hover:font-bold" href="/">
+            <li>Home</li>
           </a>
-        </li>
+          <a className="hover:font-bold" href="/">
+            <li>About </li>
+          </a>
+
+          <li>
+            <a className="hover:font-bold transition-all" href="/">
+              Contact
+            </a>
+          </li>
+        </div>
       </nav>
     </div>
   );
